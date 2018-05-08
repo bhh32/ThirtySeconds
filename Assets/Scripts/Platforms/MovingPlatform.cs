611dac80f724d283b4c3b82969612f92106e0d49
Evan Knebel <s179060@students.aie.edu.au>
2018-05-07 17:59:51 -0700

    Moving platforms

---
+++ Assets/Scripts/Platforms/MovingPlatform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public bool isVertical;
+    public float moveSpeed;
+    public Transform[] points;
+
+    private void Update()
+    {
+        if (isVertical == true)
+        {
+            if (transform.localPosition.y >= points[1].transform.localPosition.y
+                ||
+                transform.localPosition.y <= points[0].transform.localPosition.y)
+            {
+                moveSpeed *= -1;
+            }
+        }
+        else
+        {
+            if (transform.localPosition.y >= points[1].transform.localPosition.x
+                ||
+                transform.localPosition.y <= points[0].transform.localPosition.x)
+            {
+                moveSpeed *= -1;
+            }
+        }
+
+        transform.Translate(0f, moveSpeed * Time.deltaTime, 0f);
+    }
+}
